378f424f59f963d5b9debc42174f91d1e11a4c5f
Juan Amedh Sanchez Salas <juanamedh.usa@gmail.com>
2021-07-22 18:10:17 -0700

    Defined "HelloWorld" class

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,15 @@ import unittest
 from pyunitreport import HTMLTestRunner
 from seleniumm import webdriver
 
+class HelloWorld(unittest.TestCase):
+    
+    def setUp(self):
+        return super().setUp()
+
+    def test_hello_world(self):
+        pass
+
+    def tearDown(self):
+        return super().tearDown()
+
+    
