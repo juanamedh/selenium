a02b635e304382f2076ea5c1741220b9cdf71f89
Juan Amedh Sanchez Salas <juanamedh.usa@gmail.com>
2021-10-26 10:19:01 -0700

    finished first automatied test.

--- hello_world.py
+++ hello_world.py
@@ -4,17 +4,19 @@ from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
     
-    def setUp(self):
-        self.driver = webdriver.Chrome(executable_path = './chromedriver')
-        driver = self.driver
+    @classmethod
+    def setUp(cls):
+        cls.driver = webdriver.Chrome(executable_path = './chromedriver')
+        driver = cls.driver
         driver.implicitly_wait(10)
 
-    def test_hello_world(self):
-        driver = self.driver
+    def test_hello_world(cls):
+        driver = cls.driver
         driver.get('https://www.platzi.com')
 
-    def tearDown(self):
-        self.driver.quit()
+    @classmethod
+    def tearDown(cls):
+        cls.driver.quit()
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name = 'hello-world-report' ))
